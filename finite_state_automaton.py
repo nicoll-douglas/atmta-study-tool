535d5249da3875c263c0030e6194856a0265b17a
Nicoll Douglas <dev.nicoll.douglas@gmail.com>
2026-02-25 13:32:26 +0000

    add epsilon transition compatibility

--- finite_state_automaton.py
+++ finite_state_automaton.py
@@ -2,10 +2,12 @@ from __future__ import annotations
 from collections import defaultdict, deque
 from collections.abc import Set
 from graphviz import Digraph
-from typing import Generic
-import pprint
 
 class FSA:
+    """Represents a finite-state automaton (FSA) and contains algorithms that operate on it."""
+
+    EPSILON: str = ""
+
     type Alphabet = set[str]
     type Transition = tuple[State, str, State]
     type TransitionTable = defaultdict[tuple[State, str], set[State]]
@@ -25,20 +27,23 @@ class FSA:
         initial_state: State,
         final_states: set[State]
     ):
-        # independent variables, no validation needed
+        # assign initial state without validation
         self._initial_state = initial_state
+
+        # validate and assign alphabet and states without side effects on attributes that don't yet exist
+        self._validate_alphabet(alphabet)
         self._alphabet = alphabet
-
-        error_msg: str | None = self._validate_states(states)
-
-        if error_msg is not None: raise ValueError(error_msg)
-
+        self._validate_states(states)
         self._states = states
+
+        # validate and assign final states and transitions
         self.final_states = final_states
         self.transitions = transitions
 
     class State:
-        label: str
+        """Represents a state in the FSA."""
+
+        label: str # the state's arbitrary label for diagrams
 
         def __init__(self, label: str | Set[FSA.State]):
             if isinstance(label, str):
@@ -48,25 +53,24 @@ class FSA:
 
     @property
     def states(self) -> set[State]:
+        """Get the FSA's set of states."""
         return self._states
 
     @states.setter
     def states(self, incoming_states: set[State]) -> None:
-        """Set the states of the automaton.
+        """Set the states of the FSA.
 
-        As a side effect, the method removes any final states not in the new set of states to preserve integrity.
+        As a side effect, the method removes any final states and transitions containing states not in the new set of states to preserve integrity.
         
         Raises:
-            ValueError: If the set of new states does not contain the current initial state or if the given labels for the new states are not unique
+            ValueError: If the new set of states does not contain the current initial state or if the given labels for the new states are not unique.
         """
-        error_msg: str | None = self._validate_states(incoming_states)
-
-        if error_msg is not None: raise ValueError(error_msg)
+        self._validate_states(incoming_states)
         
         self.final_states -= incoming_states # remove any final states that will no longer exist
         self.transitions -= {
-            (start_state, letter, end_state)
-            for start_state, letter, end_state in self.transitions
+            (start_state, label, end_state)
+            for start_state, label, end_state in self.transitions
             if start_state not in incoming_states
             or end_state not in incoming_states
         } # remove any transitions for states that will no longer exist
@@ -74,10 +78,19 @@ class FSA:
 
     @property
     def initial_state(self) -> State:
+        """Get the initial state of the FSA."""
         return self._initial_state
 
     @initial_state.setter
     def initial_state(self, new_initial: State) -> None:
+        """Set the initial state of the FSA.
+
+        Args:
+            new_initial: A state in the current set of states.
+
+        Raises:
+            ValueError: If the new initial state is not in the current of states.
+        """
         if new_initial not in self.states:
             raise ValueError("The initial state must be in the set of states")
 
@@ -85,11 +98,15 @@ class FSA:
 
     @property
     def final_states(self) -> set[State]:
+        """Get the final states of the FSA."""
         return self._final_states
 
     @final_states.setter
     def final_states(self, incoming_final_states: set[State]) -> None:
-        """Set the automaton's final states.
+        """Set the final states of the FSA. 
+
+        Args:
+            incoming_final_states: A subset of the current set of states.
 
         Raises:
             ValueError: If any new final state is not in the current set of states.
@@ -101,56 +118,123 @@ class FSA:
     
     @property
     def alphabet(self) -> Alphabet:
+        """Get the alphabet of the FSA."""
         return self._alphabet
 
     @alphabet.setter
     def alphabet(self, incoming_alphabet: Alphabet) -> None:
-        """Set the alphabet of the automaton.
+        """Set the alphabet of the FSA.
 
-        As a side effect, the method removes transitions from the automaton where a transition's letter is not in the new alphabet to preserve integrity.
+        As a side effect, the method removes transitions from the FSA where a transition's label is not in the new alphabet and not equal to epsilon to preserve integrity.
+
+        Args:
+            incoming_alphabet: Must be a set of strings such that all strings are of length 1.
+
+        Raises:
+            ValueError: If there is any letter in given alphabet that isn't of length 1.
         """
+        self._validate_alphabet(incoming_alphabet)
+
         self.transitions -= {
-            (start_state, letter, end_state)
-            for start_state, letter, end_state in self.transitions
-            if letter not in incoming_alphabet
+            (start_state, label, end_state)
+            for start_state, label, end_state in self.transitions
+            if label not in incoming_alphabet
+            and label != self.__class__.EPSILON
         } # remove any transitions with letters that will no longer exist
 
         self._alphabet = incoming_alphabet
 
     @property
     def transitions(self) -> set[Transition]:
+        """Get the transitions of the FSA."""
         return self._transitions
 
     @transitions.setter
     def transitions(self, incoming_transitions: set[Transition]) -> None:
-        """Set the transitions of the automaton.
+        """Set the transitions of the FSA.
+
+        Args:
+            incoming_transitions: A new set of transitions constructed from states in the current set of states and letters in the current alphabet or epsilon.
 
         Raises:
-            ValueError: If there is at least 1 incoming transition such that its start or end states are not in the current set of states or its letter is not in the current alphabet.
+            ValueError: If there is at least 1 incoming transition such that its start or end states are not in the current set of states, or, its label is not in the current alphabet or not equal to epsilon.
         """
         if any(
             start_state not in self.states
             or end_state not in self.states
-            or letter not in self.alphabet
-            for start_state, letter, end_state in incoming_transitions
-        ): raise ValueError("All transition states must be in the set of states and all transition letters must be in the alphabet")
+            or (label not in self.alphabet and label != self.__class__.EPSILON)
+            for start_state, label, end_state in incoming_transitions
+        ): raise ValueError("All transition states must be in the set of states, and all transition labels must be a letter in the alphabet or epsilon")
     
         self._transitions = incoming_transitions
    
     @property
-    def deterministic(self) -> bool:
-        return all(len(end_states) <= 1 for end_states in self.transition_table.values()) 
+    def is_dfa(self) -> bool:
+        """Return a boolean flag indicating whether the FSA is a deterministic FSA (DFA) or not."""
+        return all(
+            label != self.__class__.EPSILON
+            and len(end_states) <= 1
+            for (_, label), end_states in self.transition_table.items()
+        )
+    
+    @property
+    def is_complete_dfa(self) -> bool:
+        """Return a boolean flag indicating whether the FSA is a complete DFA.
+
+        A complete DFA must have exactly 1 outgoing transition for every combination of state and letter.
+        """
+        transition_table: FSA.TransitionTable = self.transition_table
+        
+        return self.is_dfa and all(
+            len(transition_table[(state, letter)]) == 1
+            for letter in self.alphabet
+            for state in self.states
+        )
+
+    @property
+    def is_nfa(self) -> bool:
+        """Return a boolean flag indicating whether the FSA is a non-deterministic FSA (NFA) or not."""
+        return not self.is_dfa
+
+    @property
+    def is_epsilon_nfa(self) -> bool:
+        """Return a boolean flag indicating whether the FSA is an epsilon-NFA or not."""
+        return self.is_nfa and any(
+            label == self.__class__.EPSILON
+            for _, label, _ in self.transitions
+        )
+
+    @property
+    def type(self) -> str:
+        """Get a string description of the type of the FSA."""
+        if self.is_complete_dfa: return "Complete DFA"
+        if self.is_dfa: return "DFA"
+        if self.is_epsilon_nfa: return "Epsilon-NFA"
+
+        return "NFA"
 
     @property
     def transition_table(self) -> TransitionTable:
-        al: FSA.TransitionTable = defaultdict(set) 
+        """Get the transition table of the FSA.
 
-        for start_state, letter, end_state in self.transitions:
-            al[(start_state, letter)].add(end_state)
+        The transition table consists of a dictionary mapping column and row pairs (start states and labels which form a transition) to an entry in the table (the set of possible states that can be reached after the transition).
+        """
+        transition_table: FSA.TransitionTable = defaultdict(set) 
 
-        return al 
+        for start_state, label, end_state in self.transitions:
+            transition_table[(start_state, label)].add(end_state)
+
+        return transition_table
 
     def epsilon_closure(self, starting_state: State) -> set[State]:
+        """Get the epsilon-closure of a state in the FSA.
+
+        Args:
+            starting_state: The starting state from which to start considering epsilon transitions.
+
+        Returns:
+            set[State]: The epsilon-closure containing all states that can be reached by only following epsilon-transitions from the given state. At minimum this will be a set of length 1 including the given state.
+        """
         if starting_state not in self.states: raise ValueError("Starting state must be in the set of states to find the epsilon-closure")
 
         transition_table: FSA.TransitionTable = self.transition_table
@@ -159,7 +243,7 @@ class FSA:
 
         while queue:
             current: FSA.State = queue.popleft()
-            next_states: set[FSA.State] = transition_table[(current, "")]
+            next_states: set[FSA.State] = transition_table[(current, self.__class__.EPSILON)]
 
             for state in next_states:
                 if state not in closure:
@@ -169,7 +253,14 @@ class FSA:
         return closure 
 
     def subset_construction(self) -> FSA:
-        if self.deterministic: return self
+        """Construct an equivalent deterministic FSA from the current FSA via the subset construction algorithm.
+
+        The method does not guarantee that the resulting FSA will be a complete DFA, only a DFA.
+
+        Returns:
+            FSA: An equivalent deterministic FSA or a reference to the FSA itself if already deterministic.
+        """
+        if self.is_dfa: return self
 
         # step 1: get the NFA transition table and DFA initial state (NFA epsilon closure)
         nfa_transition_table: FSA.TransitionTable = self.transition_table
@@ -185,7 +276,7 @@ class FSA:
 
             for letter in self.alphabet:
                 # step 2.1: find all reachable states in the NFA from the set of NFA states in the current DFA states
-                reachable_states: frozenset[FSA.State] = set()
+                reachable_states: set[FSA.State] = set()
 
                 for nfa_state in current_dfa_state:
                     for transition_state in nfa_transition_table[(nfa_state, letter)]:
@@ -208,7 +299,7 @@ class FSA:
         }
 
         return FSA(
-            alphabet=self.alphabet.copy(),
+            alphabet=self.alphabet,
             initial_state=dfa_state_map[dfa_initial_state],
             final_states={
                 dfa_state_map[dfa_state]
@@ -217,30 +308,41 @@ class FSA:
             },
             states=set(dfa_state_map.values()),
             transitions={
-                (dfa_state_map[start_state], letter, dfa_state_map[end_state])
-                for (start_state, letter), end_state in dfa_transition_table.items()
+                (dfa_state_map[start_state], label, dfa_state_map[end_state])
+                for (start_state, label), end_state in dfa_transition_table.items()
             }
         )
        
-    def _validate_states(self, states: set[State]) -> str | None:
-        """Validate whether the given set of states is a valid assignment for the automaton's states.
+    def _validate_states(self, states: set[State]) -> None:
+        """Validate whether the given set of states is a valid assignment for the FSA's states.
 
-        Returns:
-            None | str: None if validation passed or an error message if validation failed.
-        
         Raises:
-            ValueError: If the set of new states does not contain the current initial state or if the given labels for the new states are not unique
+            ValueError: If the current initial state is not in the given set of states or the states don't have unique labels.
         """
         if self.initial_state not in states:
-            return "The initial state must be in the set of states"
+            raise ValueError("The initial state must be in the set of states")
 
         if len([state.label for state in states]) != len({state.label for state in states}):
-            return "All states must have unique labels"
-        
+            raise ValueError("All states must have unique labels")
+
         return None
 
+    def _validate_alphabet(self, alphabet: Alphabet) -> None:
+        """Validate whether the given alphabet is a valid assignment for the FSA's alphabet.
+
+        Raises:
+            ValueError: If the alphabet contains 1 or more strings that are not of length 1.
+        """
+        if any(len(letter) != 1 for letter in alphabet):
+            raise ValueError("All letters in the alphabet must be a string of length 1")
 
     def draw(self, filename: str, open_file: bool = True) -> None:
+        """Create an image representation of the FSA and optionally open the image file.
+
+        Args:
+            filename: The extension-less filename to give the .png file.
+            open_file: Whether to open the .png file for immediate viewing.
+        """
         graph: Digraph = Digraph("FSA", format="png")
 
         graph.attr(rankdir="LR")
@@ -250,28 +352,37 @@ class FSA:
 
             graph.node(state.label, shape=shape)
 
-        for start_state, letter, end_state in self.transitions:
-            graph.edge(start_state.label, end_state.label, label=letter)
+        for start_state, label, end_state in self.transitions:
+            graph.edge(start_state.label, end_state.label, label=label)
 
         graph.node("start", label="", shape="none", width="0", height="0")
         graph.edge("start", self.initial_state.label)
+        graph.render(filename, view=open_file, cleanup=True)
 
-        graph.render(filename, view=open_file)
+def example_1() -> None:
+    q: list[FSA.State] = [FSA.State(f"q{i}") for i in range(4)] # list of the FSA's states for quick positional access
+    fsa: FSA = FSA(
+        states=set(q),
+        initial_state=q[0],
+        final_states={q[3]},
+        alphabet={"0", "1", "2"},
+        transitions={
+            (q[0], "0", q[1]),
+            (q[1], "1", q[1]),
+            (q[1], "1", q[2]),
+            (q[1], "1", q[3]),
+            (q[2], "1", q[3]),
+            (q[2], "2", q[2]),
+            (q[3], "0", q[2])
+        }
+    )
 
+    print("Example 1:", fsa.type)
+    fsa.draw("example_1")
 
+    dfa_equivalent: FSA = fsa.subset_construction()
 
+    print("Example 1 Equivalent DFA:", dfa_equivalent.type)
+    dfa_equivalent.draw("example_1_dfa_equivalent")
 
-q0: FSA.State = FSA.State("q0")
-q1: FSA.State = FSA.State("q1")
-
-fsa: FSA = FSA(
-    states={q0, q1},
-    initial_state=q0,
-    final_states=set(),
-    alphabet={"a", "b"},
-    transitions={
-        (q0, "a", q1),
-    }
-)
-
-fsa.subset_construction().draw("fsa_output_2")
+example_1()
