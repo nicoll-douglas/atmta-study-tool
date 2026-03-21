c5a65d1d0d740e8235b93db2444e621030a1e432
Nicoll Douglas <dev.nicoll.douglas@gmail.com>
2026-03-21 18:22:22 +0000

    update

--- fsa/_hooks/alphabet.py
+++ fsa/_hooks/alphabet.py
@@ -1,39 +1,31 @@
-from ..models.fsa_renderer import FSARenderer
-from typing import AbstractSet, TYPE_CHECKING
+from typing import TYPE_CHECKING
+
 
 if TYPE_CHECKING:
-    from ..models.transition_table import _TransitionTable
+    from ..models.transition_table import TransitionTable
+    from models.symbol import Symbol
+    from models.alphabet import Alphabet
 
 
-# hook function to run before a symbol is added to the alphabet of an FSA
-def pre_add(new_symbol: str) -> None:
-    """Validate that the new symbol being added to an alphabet is valid."""
-    if new_symbol == FSARenderer.EPSILON_LABEL:
-        raise ValueError(
-            f"Expected a string not equal to "
-            f"'{FSARenderer.EPSILON_LABEL}' "
-            f"(U+{ord(FSARenderer.EPSILON_LABEL):04X}). Got '{new_symbol}'."
-        )
+def post_discard(symbol: Symbol, transition_table: TransitionTable) -> None:
+    """Remove transitions from the given transition table if they utilise the given symbol.
 
-    if len(new_symbol) != 1:
-        raise ValueError("Expected a string of length 1. " f"Got '{new_symbol}'.")
+    This hook is intended to run after the symbol is discarded from an FSA's alphabet.
+
+    Args:
+        symbol: The symbol discarded from the FSA's alphabet.
+        transition_table: The transition table of the FSA.
+    """
+    transition_table.remove_such_that(lambda key, _: symbol == key[1])
 
 
-# hook function to run after a symbol is removed from the alphabet of an FSA
-def post_discard(symbol: str, current_transition_table: _TransitionTable) -> None:
-    """Remove all transitions from the given transition table if they
-    utilise the symbol removed from the alphabet."""
-    current_transition_table.remove_such_that(
-        lambda _, transition_symbol, __: symbol == transition_symbol
-    )
+def post_set(alphabet: Alphabet, transition_table: TransitionTable) -> None:
+    """Remove transitions from the given transition table that utilise any symbol not in the given alphabet.
 
+    This hook is intended to run after the alphabet is set for an FSA.
 
-# hook function to run after an alphabet is set for an FSA
-def post_set(
-    new_alphabet: AbstractSet[str], current_transition_table: _TransitionTable
-) -> None:
-    """Remove all transitions from the given transition table if they
-    don't utilise any symbol in the new alphabet."""
-    current_transition_table.remove_such_that(
-        lambda _, symbol, __: symbol not in new_alphabet
-    )
+    Args:
+        alphabet: An alphabet set for an FSA.
+        transition_table: The transition table of the FSA.
+    """
+    transition_table.remove_such_that(lambda key, _: key[1] not in alphabet)
--- models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .alphabet import Alphabet
-from .symbol import Symbol
-from .word import Word, Epsilon
-
-__all__ = ["Alphabet", "Symbol", "Word", "Epsilon"]
