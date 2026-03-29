f1f16d3c709a7e1b7bd00f2b4c1e00c488ee68c3
Nicoll Douglas <dev.nicoll.douglas@gmail.com>
2026-03-29 21:39:41 +0100

    remove automata package

--- src/atmta_study_tool/automata/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .state import State
-from .transition_table import TransitionTable
-
-__all__ = ["State", "TransitionTable"]
--- src/atmta_study_tool/automata/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from .state import *
-from .transition import *
-
-__all__ = ["state_from", "states_from", "validate_epsilon", "transition_from"]
